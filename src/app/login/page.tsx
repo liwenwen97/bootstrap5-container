56089de79e23807514f6c38d491b0b8d3c55e637
liwenwen <1457910890@qq.com>
2025-03-07 17:48:28 +0800

    新增三端预览

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,7 @@
 import React from 'react'
-import { GoogleLogin, CredentialResponse } from '@react-oauth/google'
 
 export default function Page() {
-  const handleLoginSuccess = (credentialResponse: CredentialResponse) => {
-    console.log('Login Success:', credentialResponse);
-    // 在这里处理登录成功后的逻辑
-  };
-
-  const handleLoginFailure = () => {
-    console.error('Login Failed');
-    // 在这里处理登录失败后的逻辑
-  };
-
   return (
-    <div>
-        <h1>Login</h1>
-        <GoogleLogin
-          onSuccess={handleLoginSuccess}
-          onError={handleLoginFailure}
-        />
-    </div>
+    <div>page</div>
   )
 }
